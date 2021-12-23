c9186a583b00a0191de9e5434b0b203b241ab1c1
Manan-2712 <behlmanan202@gmail.com>
2021-12-23 21:57:30 +0530

    invertBinaryTree

---
+++ invertTree.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/invert-binary-tree/submissions/
+public class invertTree {
+    public TreeNode invertTree(TreeNode root) {
+         if(root== null){
+             return root;
+         }
+        root.left=invertTree(root.left);
+        root.right= invertTree(root.right);
+        if(root.left == null){
+            root.left= root.right;
+            root.right= null;
+        }
+        else if(root.right == null){
+            root.right= root.left;
+             root.left= null;
+        }
+        
+        else
+        {
+            if(root.left !=null &&  root.right!= null){
+            TreeNode temp= root.left;
+            root.left = root.right;
+            root.right = temp;
+        }
+     
+        
+    }
+        return root;
+}
+}
+
