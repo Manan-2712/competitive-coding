8b30f8c35a44ecdd295266091c46be979f5fe8de
Manan-2712 <behlmanan202@gmail.com>
2022-01-16 15:09:57 +0530

    QueueReversal

--- codeproblems/queuereversal.java
+++ codeproblems/queuereversal.java
@@ -1,19 +1,20 @@
 package codeproblems;
-import java.util.Queue;
-public class queuereversal {
-  //  import java.util.Queue;
 
-    public static void reverse(Queue < Integer > q) {
-        if(q.isEmpty()){
-            return;
-        }
-        //  Queue<Integer>q = new LinkedList<>();
-          int n = q.size();
-    //    for(int i=0;i<n;i++){
-           int temp = q.peek();
-            q.remove();
-            reverse(q);
-            q.add(temp);
-       // }
+import java.util.Queue;
+
+public class queuereversal {
+
+  public static void reverse(Queue<Integer> q) {
+    if (q.isEmpty()) {
+      return;
     }
+
+    int n = q.size();
+
+    int temp = q.peek();
+    q.remove();
+    reverse(q);
+    q.add(temp);
+
+  }
 }
