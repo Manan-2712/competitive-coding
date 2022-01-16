1781ed34024fa423e38d666c531d777c661ac650
Manan-2712 <behlmanan202@gmail.com>
2022-01-16 15:05:54 +0530

    InvertTree

--- codeproblems/invertTree.java
+++ codeproblems/invertTree.java
@@ -1,32 +1,29 @@
-package codeproblems;
 //https://leetcode.com/problems/invert-binary-tree/submissions/
-public class invertTree {
-    public TreeNode invertTree(TreeNode root) {
-         if(root== null){
-             return root;
-         }
-        root.left=invertTree(root.left);
-        root.right= invertTree(root.right);
-        if(root.left == null){
-            root.left= root.right;
-            root.right= null;
-        }
-        else if(root.right == null){
-            root.right= root.left;
-             root.left= null;
-        }
-        
-        else
-        {
-            if(root.left !=null &&  root.right!= null){
-            TreeNode temp= root.left;
-            root.left = root.right;
-            root.right = temp;
-        }
-     
-        
-    }
-        return root;
-}
-}
+package codeproblems;
 
+public class invertTree {
+    public TreeNode invertTrees(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        root.left = invertTrees(root.left);
+        root.right = invertTrees(root.right);
+        if (root.left == null) {
+            root.left = root.right;
+            root.right = null;
+        } else if (root.right == null) {
+            root.right = root.left;
+            root.left = null;
+        }
+
+        else {
+            if (root.left != null && root.right != null) {
+                TreeNode temp = root.left;
+                root.left = root.right;
+                root.right = temp;
+            }
+
+        }
+        return root;
+    }
+}
