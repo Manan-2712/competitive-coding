693345cdc6202d2c26b231d1cb40b85579c51ac9
Manan-2712 <behlmanan202@gmail.com>
2022-01-31 00:52:54 +0530

    commits

---
+++ codeproblems/reverseinKgroups.java
@@ -0,0 +1,38 @@
+//https://www.codingninjas.com/codestudio/problems/reverse-list-in-k-groups_983644#:~:text=1.,2.
+
+package codeproblems;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class reverseinKgroups {
+
+    public static Node kReverse(Node head, int k) {
+        if (head == null) {
+            return null;
+        }
+        Node next = null;
+        Node prev = null;
+        Node curr = head;
+        int count = 0;
+        while (curr != null && count < k) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        if (next != null) {
+            head.next = kReverse(next, k);
+        }
+        return prev;
+
+    }
+}
