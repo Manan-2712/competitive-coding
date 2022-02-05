138a702240a9edc7f6996374aa859dace5a45dce
Manan-2712 <behlmanan202@gmail.com>
2022-02-05 17:11:28 +0530

    Linkedlistdetect and remove cycle

---
+++ codeproblems/Detect_and_remove_cycle.java
@@ -0,0 +1,49 @@
+package codeproblems;
+
+// https://www.codingninjas.com/codestudio/problems/interview-shuriken-42-detect-and-remove-loop_241049?leftPanelTab=1
+public class Detect_and_remove_cycle {
+
+    public static class Node {
+
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node removeLoop(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null) {
+            return head;
+        }
+        slow = head;
+        if (slow != fast) {
+            while (slow.next != fast.next) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            fast.next = null;
+        } else {
+            while (fast.next != slow) {
+                fast = fast.next;
+            }
+            fast.next = null;
+
+        }
+        return head;
+    }
+}
