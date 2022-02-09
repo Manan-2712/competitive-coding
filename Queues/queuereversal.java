eca453f3257ee4dfda2900f5be7796ca5875db9e
Manan-2712 <behlmanan202@gmail.com>
2022-02-10 00:52:54 +0530

    Queue reversal

---
+++ Queues/queuereversal.java
@@ -0,0 +1,20 @@
+package Queues;
+
+import java.util.Queue;
+
+public class queuereversal {
+
+  public static void reverse(Queue<Integer> q) {
+    if (q.isEmpty()) {
+      return;
+    }
+
+    int n = q.size();
+
+    int temp = q.peek();
+    q.remove();
+    reverse(q);
+    q.add(temp);
+
+  }
+}
