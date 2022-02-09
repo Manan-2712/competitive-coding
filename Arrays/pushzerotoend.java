bc3f537e7c29bc6ef3ef1d77ecfd924b5eca5291
Manan-2712 <behlmanan202@gmail.com>
2022-02-10 00:35:04 +0530

    Push zeroes to end

--- Arrays/pushzerotoend.java
+++ Arrays/pushzerotoend.java
@@ -1,8 +1,8 @@
 //https://www.codingninjas.com/codestudio/problems/push-zeros-to-end_583?leftPanelTab=1
 package Arrays;
 public class pushzerotoend {
-    // using array rotation concept
-    public static void pushZerosAtEnd(int[] arr) {
+ 
+    public static void pushZerosAtEnd(int[] arr) { // using arrayrotation concept
         int rot = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
