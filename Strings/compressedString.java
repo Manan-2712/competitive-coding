01e1f41179b17ef63b6e95cbcdd516ebb1bca7d6
Manan-2712 <behlmanan202@gmail.com>
2022-02-10 00:45:19 +0530

    String compression ,multiplication of string s

---
+++ Strings/compressedString.java
@@ -0,0 +1,21 @@
+//https://www.codingninjas.com/codestudio/problems/compress-the-string_526?leftPanelTab=1
+package Strings;
+
+public class compressedString {
+    public static String getCompressedString(String str) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            count++;
+            if (i == (str.length() - 1) || str.charAt(i) != str.charAt(i + 1)) {
+                if (count > 1)
+                    sb.append(str.charAt(i)).append(count);
+                else
+                    sb.append(str.charAt(i));
+                count = 0;
+            }
+        }
+        return sb.toString();
+    }
+
+}
