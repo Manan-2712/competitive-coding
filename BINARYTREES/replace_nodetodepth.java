dde3561f76f6253b93089fc1f4148c33b0d6ccac
Manan-2712 <behlmanan202@gmail.com>
2022-02-27 20:10:03 +0530

    replace node data with depth of a tree

---
+++ BINARYTREES/replace_nodetodepth.java
@@ -0,0 +1,36 @@
+package BINARYTREES;
+
+public class replace_nodetodepth {
+
+
+    class BinaryTreeNode<Integer> {
+        int data;
+        BinaryTreeNode<Integer> left;
+        BinaryTreeNode<Integer> right;
+
+        public BinaryTreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+
+
+    public static BinaryTreeNode<Integer> helper(BinaryTreeNode<Integer> root,int level){
+        if(root==null){
+            return null;
+        }
+        root.data=level;
+        BinaryTreeNode<Integer> left = helper(root.left,level+1);
+        BinaryTreeNode<Integer> right = helper(root.right,level+1);
+        return root;
+        
+        
+    }
+    public static BinaryTreeNode<Integer> changeToDepthTree(BinaryTreeNode<Integer> root) {
+        BinaryTreeNode<Integer> roots = helper(root,0);
+        return roots;
+    }
+}
+
+    
+
