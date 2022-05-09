47f3add966109076fdece9eae564e0d04935125b
Manan-2712 <behlmanan202@gmail.com>
2022-05-09 21:59:05 +0530

    first commit

---
+++ InterviewBit/maxsumcontigoussubarray.java
@@ -0,0 +1,22 @@
+//https://www.interviewbit.com/problems/max-sum-contiguous-subarray/
+package InterviewBit;
+
+public class maxsumcontigoussubarray {
+
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int maxSubArray(final int[] A) {
+        int cursum = 0;
+        int maxsum = Integer.MIN_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            cursum += A[i];
+            if (cursum > maxsum) {
+                maxsum = cursum;
+            }
+            if (cursum < 0) {
+                cursum = 0;
+            }
+        }
+        return maxsum;
+    }
+
+}
