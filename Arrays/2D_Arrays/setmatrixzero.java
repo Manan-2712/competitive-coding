f01b33eb22145ff1d068ef9836323418b3cb9f71
Manan-2712 <behlmanan202@gmail.com>
2023-04-07 11:21:11 +0530

    set matrix zero

---
+++ Arrays/2D_Arrays/setmatrixzero.java
@@ -0,0 +1,69 @@
+//https://leetcode.com/problems/set-matrix-zeroes/description/
+public class setmatrixzero {
+
+
+    
+    // BRUTE FORCE ONE(NOT SPACE EFFICIENT)
+    // public void setZeroes(int[][] matrix) {
+    // int m = matrix.length;
+    // int n = matrix[0].length;
+    // Set<Integer> rows= new HashSet<Integer> ();
+    // Set<Integer> cols = new HashSet<Integer>();
+    // for(int i=0;i<m;i++){
+    // for(int j=0;j<n;j++){
+    // if(matrix[i][j]==0){
+    // rows.add(i);
+    // cols.add(j);
+    // }
+    // }
+    // }
+
+    // for(int i=0;i<m;i++){
+    // for(int j=0;j<n;j++){
+    // if(rows.contains(i)|| cols.contains(j)){
+    // matrix[i][j]=0;
+    // }
+    // }
+    // }
+    // }
+
+    // OPTIMAL SOLUTION (SPACE EFFICIENT )
+    // TIME COMPLEXITY = 0(N^2)
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[j][0] = 0;
+            }
+        }
+
+    }
+}
