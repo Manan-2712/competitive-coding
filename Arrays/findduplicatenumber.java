f62415a21b0a3a139a83414d8fe225e60dcf0b90
Manan-2712 <behlmanan202@gmail.com>
2023-04-10 17:59:21 +0530

    find repeating using floyyd detection method

---
+++ Arrays/findduplicatenumber.java
@@ -0,0 +1,21 @@
+package Arrays;
+//https://leetcode.com/problems/find-the-duplicate-number/description/
+//https://www.youtube.com/watch?v=32Ll35mhWg0&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=2
+public class findduplicatenumber {
+    // using floydd detection method
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+
+    }
+}
