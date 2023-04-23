a2b3666371ae710fb892ce41cbeefd6d61184938
Manan-2712 <behlmanan202@gmail.com>
2023-04-23 19:09:02 +0530

    trapping rainwater problem optimal approach

---
+++ Arrays/trappingrainwaterproblem.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/trapping-rain-water/description/
+//Leetcode 42.
+public class trappingrainwaterproblem {
+    public int trap(int[] height) {
+        int n = height.length;
+        int left = 0;
+        int right = n - 1;
+        int maxleft = 0;
+        int maxright = 0;
+        int res = 0;
+        while (left <= right) {
+            if (height[left] <= height[right]) {
+                if (height[left] > maxleft) {
+                    maxleft = height[left];
+                } else {
+                    res += maxleft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxright) {
+                    maxright = height[right];
+                } else {
+                    res += maxright - height[right];
+                }
+                right--;
+            }
+
+        }
+        return res;
+
+    }
+}
