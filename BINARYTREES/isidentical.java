7c33d46640fcbda5c1d51754f29dffc1ed1b24f4
Manan-2712 <behlmanan202@gmail.com>
2023-07-11 16:50:44 +0530

    isidentical or not

---
+++ BINARYTREES/isidentical.java
@@ -0,0 +1,39 @@
+package BINARYTREES;
+
+//https://leetcode.com/problems/same-tree/description/
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class isidentical {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        boolean isleft = isSameTree(p.left, q.left);
+        boolean isright = isSameTree(p.right, q.right);
+        return isleft && isright;
+    }
+}
