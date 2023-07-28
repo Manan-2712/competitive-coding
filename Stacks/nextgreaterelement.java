738886866c176ac464d3426440889d3fbb4b7cdb
Manan-2712 <behlmanan202@gmail.com>
2023-07-29 00:34:40 +0530

    NEXT greater element using stack

---
+++ Stacks/nextgreaterelement.java
@@ -0,0 +1,22 @@
+package Stacks;
+//https://leetcode.com/problems/next-greater-element-i/description/
+
+public class nextgreaterelement {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            int greaterIdx = -1, j = nums2.length - 1;
+            while (j >= 0 && nums2[j] != nums1[i]) {
+                if (nums2[j] > nums1[i]) {
+                    greaterIdx = nums2[j];
+                }
+                j--;
+            }
+            ans[i] = greaterIdx;
+        }
+
+        return ans;
+
+    }
+}
