5c2099bd0a90d72ab0346e369d7e26851d3ed4f8
Manan-2712 <behlmanan202@gmail.com>
2023-08-09 00:10:33 +0530

    reverse words of string

---
+++ Strings/reversewordsofstring.java
@@ -0,0 +1,29 @@
+package Strings;
+
+//https://leetcode.com/problems/reverse-words-in-a-string/description/
+public class reversewordsofstring {
+    public String reverseWords(String s) {
+        int i = s.length() - 1;
+        String ans = "";
+        while (i >= 0) {
+            while (i >= 0 && s.charAt(i) == ' ') {
+                i--;
+            }
+            int j = i;
+            if (i < 0) {
+                break;
+            }
+            while (i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+
+            if (ans.isEmpty()) {
+                ans = ans.concat(s.substring(i + 1, j + 1));
+            } else {
+                ans = ans.concat(" " + s.substring(i + 1, j + 1));
+            }
+        }
+        return ans;
+
+    }
+}
