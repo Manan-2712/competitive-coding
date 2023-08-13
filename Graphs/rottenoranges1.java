8c75c6b9d22f920a5f0ee72f8bf1ea5fc76b669f
Manan-2712 <behlmanan202@gmail.com>
2023-08-13 17:02:38 +0530

    implementation of LRU cache problem

--- Graphs/rottenoranges1.java
+++ Graphs/rottenoranges1.java
@@ -17,25 +17,6 @@ class Pair {
 
 public class rottenoranges1 {
     // { Driver Code Starts
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(s[0]);
-            int m = Integer.parseInt(s[1]);
-            int[][] grid = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                for (int j = 0; j < m; j++) {
-                    grid[i][j] = Integer.parseInt(S[j]);
-                }
-            }
-            Solution obj = new Solution();
-            int ans = obj.orangesRotting(grid);
-            System.out.println(ans);
-        }
-    }
 
     // } Driver Code Ends
 
