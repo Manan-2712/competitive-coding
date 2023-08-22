12cea5781bd30bef25f35609c686257fd58b51f4
Manan-2712 <behlmanan202@gmail.com>
2023-08-22 22:13:28 +0530

    valid anangrams

---
+++ Strings/validanagrams.java
@@ -0,0 +1,22 @@
+package Strings;
+import java.util.*;
+// https://leetcode.com/problems/valid-anagram/description/
+public class validanagrams {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
